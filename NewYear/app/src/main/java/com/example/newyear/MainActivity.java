8693844158e69689aae2632589bd1ae3e90022fa
HongDang1 <dang.phamhong1905@gmail.com>
2024-09-26 13:57:05 +0700

    Update MainActivity.java

--- NewYear/app/src/main/java/com/example/newyear/MainActivity.java
+++ NewYear/app/src/main/java/com/example/newyear/MainActivity.java
@@ -1,24 +1,39 @@
 package com.example.newyear;
 
 import android.os.Bundle;
-
-import androidx.activity.EdgeToEdge;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    String[] canArray = {"Canh", "Tân", "Nhâm", "Quý", "Giáp", "Ất", "Bính", "Đinh", "Mậu", "Kỷ"};
+    String[] chiArray = {"Thân", "Dậu", "Tuất", "Hợi", "Tý", "Sửu", "Dần", "Mão", "Thìn", "Tỵ", "Ngọ", "Mùi"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        EditText gregorianYearInput = findViewById(R.id.editnamduonglich);
+        Button convertButton = findViewById(R.id.button1);
+        TextView lunarYearOutput = findViewById(R.id.textView5);
+
+        convertButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int gregorianYear = Integer.parseInt(gregorianYearInput.getText().toString());
+                String lunarYear = convertToLunarYear(gregorianYear);
+                lunarYearOutput.setText(lunarYear);
+            }
         });
+    }
+
+    private String convertToLunarYear(int year) {
+        String can = canArray[year % 10];
+        String chi = chiArray[year % 12];
+        return can + " " + chi;
     }
 }
