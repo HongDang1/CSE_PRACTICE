a729ca0ea674530000f8348c2ccd77f9d0542838
HongDang1 <dang.phamhong1905@gmail.com>
2024-10-01 09:52:11 +0700

    Update MainActivity.java

--- Information/app/src/main/java/com/example/information/MainActivity.java
+++ Information/app/src/main/java/com/example/information/MainActivity.java
@@ -1,24 +1,141 @@
 package com.example.information;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
-
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.Toast;
+import android.Manifest;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+
 public class MainActivity extends AppCompatActivity {
+    EditText edtten, editCMND, editBosung;
+    CheckBox chkdocbao, chkdocsach, chkdoccode;
+    Button btnsend;
+    RadioGroup group;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        edtten = findViewById(R.id.editHoten);
+        editCMND = findViewById(R.id.editCMND);
+        editBosung = findViewById(R.id.editBosung);
+        chkdocbao = findViewById(R.id.chkdocbao);
+        chkdoccode = findViewById(R.id.chkdoccoding);
+        chkdocsach = findViewById(R.id.chkdocsach);
+        btnsend = findViewById(R.id.btnGuitt);
+
+        btnsend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                doShowInformation();
+            }
         });
+    }
+
+    public void doShowInformation() {
+        // Kiểm tra tên hợp lệ
+        String ten = edtten.getText().toString().trim();
+        if (ten.length() < 3) {
+            edtten.requestFocus();
+            edtten.selectAll();
+            Toast.makeText(this, "Tên phải >= 3 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Kiểm tra CMND hợp lệ
+        String cmnd = editCMND.getText().toString().trim();
+        if (cmnd.length() != 9) {
+            editCMND.requestFocus();
+            editCMND.selectAll();
+            Toast.makeText(this, "CMND phải đúng 9 ký tự", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Kiểm tra bằng cấp
+        int id = group.getCheckedRadioButtonId();
+        if (id == -1) {
+            Toast.makeText(this, "Phải chọn bằng cấp", Toast.LENGTH_LONG).show();
+            return;
+        }
+        RadioButton rad = findViewById(id);
+        String bang = rad.getText().toString();
+
+        // Kiểm tra sở thích
+        StringBuilder sothich = new StringBuilder();
+        if (chkdocbao.isChecked()) {
+            sothich.append(chkdocbao.getText()).append("\n");
+        }
+        if (chkdocsach.isChecked()) {
+            sothich.append(chkdocsach.getText()).append("\n");
+        }
+        if (chkdoccode.isChecked()) {
+            sothich.append(chkdoccode.getText()).append("\n");
+        }
+
+        String bosung = editBosung.getText().toString().trim();
+        if (bosung.isEmpty()) {
+            editBosung.requestFocus();
+            Toast.makeText(this, "Thông tin bổ sung không được để trống", Toast.LENGTH_LONG).show();
+            return;
+        }
+
+        // Tạo Dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Thông tin cá nhân");
+        builder.setPositiveButton("Đóng", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+
+        // Tạo nội dung
+        String msg = ten + "\n";
+        msg += cmnd + "\n";
+        msg += bang + "\n";
+        msg += sothich;
+        msg += "___________________________\n";
+        msg += "Thông tin bổ sung:\n";
+        msg += bosung + "\n";
+        msg += "___________________________";
+
+        builder.setMessage(msg); // thiết lập nội dung
+        builder.create().show(); // hiển thị Dialog
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        AlertDialog.Builder b = new AlertDialog.Builder(MainActivity.this);
+        b.setTitle("Question");
+        b.setMessage("Are you sure you want to exit?");
+        // b.setIcon(R.drawable.inform);  // Đảm bảo có biểu tượng hoặc xóa dòng này
+        b.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                finish();
+            }
+        });
+        b.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        b.create().show();
     }
 }
