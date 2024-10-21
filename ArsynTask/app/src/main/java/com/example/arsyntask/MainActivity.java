c18154099fb02231ec52096c5d590a83b87b1bcf
HongDang1 <dang.phamhong1905@gmail.com>
2024-10-21 13:49:19 +0700

    bai20

---
+++ ArsynTask/app/src/main/java/com/example/arsyntask/MainActivity.java
@@ -0,0 +1,40 @@
+package com.example.arsyntask;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnStart;
+    // Khai báo MyAsyncTask
+    MyAsyncTask mytt;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnStart = (Button) findViewById(R.id.button1);
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View arg0) {
+                // Gọi phương thức để bắt đầu AsyncTask
+                doStart();
+            }
+        });
+    }
+
+    private void doStart() {
+        // Truyền this (chính là MainActivity hiện tại) qua background Thread
+        mytt = new MyAsyncTask(this);
+        // Kích hoạt Tiến trình
+        // Khi gọi hàm này thì onPreExecute của mytt sẽ thực thi trước
+        mytt.execute();
+    }
+}
